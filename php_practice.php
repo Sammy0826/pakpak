8d55dfe8ffb7302b6f9d0dc91802433041a11398
InukiChiga <112374473+Sammy0826@users.noreply.github.com>
2025-08-28 20:15:46 +0800

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,125 @@
+<!-- Hello!
+<h1>Hello!</h1>
+<style>
+    h1{
+        color: yellow;
+    }
+</style>
+<script>
+    alert("Hello!")
+</script> -->
+<?php
+    /*
+    echo "<b>Hello World!</b>";
+    echo "<b>Hello World!</b>";
+    echo "<b>Hello World!</b>";
+    echo "<b>Hello World!</b>";
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>Hello this is Me!</h1> -->
+    <?php
+        // ECHo "Hello";
+
+        // $color = "Red";
+        // $Color = "Blue";
+        // echo $Color;
+        // echo $color;
+        
+        // $_ = 5;
+        // echo $_;
+        
+        // var_dump($color);
+        // var_dump($_);
+        // var_dump(3.1);
+        // var_dump(false);
+        // var_dump([1,2]);
+
+        //Variable
+        // $x = 5; //global
+
+        // function test(){
+        //     // $x = 10; //local
+        //     static $x = 10;
+        //     echo $x;
+        //     $x++;
+        //     echo "<br>";
+        // }
+        // test();
+        // test();
+        // test();
+        // test();
+        // test();
+        // test();
+        // echo $x;
+
+        //echo & print
+
+        // echo ("Hello"), ("Hello");
+        // echo "World!", "World!";
+        // print ("Hello");
+        // print "World!";
+
+        // $x = print("Hello");
+        // echo $x;
+
+        // $a = "Sammy";
+        // $b = 21;
+        // $c = 5.5;
+        // $d = true;
+        // $e = [1,2,3];
+        // $f = new stdClass();
+        // $g = null;
+        // $h = fopen("php_practice.php", "r");
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+        // // echo strlen($a);
+
+        // $x = "Juan Cruz";
+        // echo str_replace("Cruz", "Lancelotte", $x);
+
+        // echo trim("Hello World!            ");
+
+        // var_dump (explode("u", $x));
+
+        // echo "Hello World!";
+
+        // $x = "Hello";
+        // $y = "Love";
+        // $z = "Goodbye";
+
+        // echo $x . " " . $y . " ".  $z . "<br>";
+        // echo "$x sanaol $y beybeh $z <br";
+        // echo "Samantha \$Beybeh\" Capunpun";
+        
+
+
+        
+    ?>
+</body>
+</html>
+
